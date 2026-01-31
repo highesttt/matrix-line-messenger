50d881a984b2a8556b6810e21e9f1656989e7e6e
highesttt <github@sk.network>
2026-01-31 02:11:27 -0600

    fix: 🐛 Correct build script filename and enhance build flags for Windows

--- build-windows.sh
+++ build-windows.sh
@@ -14,7 +14,9 @@ sed -i '/"path\/filepath"/d' "$WEBSOCKET_FILE"
 sed -i '/parsed.Path = filepath.Join/c\	// Windows fix: Don'"'"'t use filepath.Join for URL paths as it uses backslashes on Windows\n\t// Use simple string concatenation to ensure forward slashes\n\tif !strings.HasSuffix(parsed.Path, "/") {\n\t\tparsed.Path += "/"\n\t}\n\tparsed.Path += "_matrix/client/unstable/fi.mau.as_sync"' "$WEBSOCKET_FILE"
 
 echo "Building matrix-line for Windows..."
-CC=x86_64-w64-mingw32-gcc go build -mod=vendor -o matrix-line.exe ./cmd/matrix-line
+MAUTRIX_VERSION=$(cat go.mod | grep 'maunium.net/go/mautrix ' | awk '{ print $2 }' | head -n1)
+GO_LDFLAGS="-s -w -X main.Tag=$(git describe --exact-match --tags 2>/dev/null) -X main.Commit=$(git rev-parse HEAD) -X 'main.BuildTime=`date -Iseconds`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'"
+CC=x86_64-w64-mingw32-gcc go build -mod=vendor -ldflags="$GO_LDFLAGS" -o matrix-line.exe ./cmd/matrix-line "$@"
 
 if [ $? -eq 0 ]; then
     echo ""
